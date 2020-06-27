ab14c96fdd53407fb0930da2920b9faab42380fa
deadman25 <62547799+deadman25@users.noreply.github.com>
2020-06-27 19:09:11 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+const path = require('path')
+const express = require('express')
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'node_modules'))); // npm
+app.use(express.static('assets')); //ajax
+
+app.get('/',function(req,res){
+ 	 res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.get('/senior',function(req,res){
+ 	 res.sendFile(path.join(__dirname + '/dist/senior.html'));
+});
+
+app.get('/college',function(req,res){
+ 	 res.sendFile(path.join(__dirname + '/dist/college.html'));
+});
+
+app.get('/requirements',function(req,res){
+ 	 res.sendFile(path.join(__dirname + '/dist/requirements.html'));
+});
+
+app.listen(port, () => {
+	console.log('server in running on port : ' + port)
+})
+
