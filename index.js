ba5e94f8efdf6fc89b18ee056969c23c233d4fae
deadman25 <62547799+deadman25@users.noreply.github.com>
2020-06-28 19:44:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000
 const path = require('path')
 const express = require('express')
 const app = express();
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 app.use(express.static(path.join(__dirname, 'node_modules'))); // npm
