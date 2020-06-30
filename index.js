a28a7d07c6c9fd5f8d3e840de78507215234016d
deadman25 <62547799+deadman25@users.noreply.github.com>
2020-06-30 11:11:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-app.use(express.static(path.join(__dirname, 'node_modules'))); // npm
+app.use(express.static(path.join(__dirname, '../node_modules'))); // npm
 app.use(express.static('assets')); //ajax
 
 app.get('/',function(req,res){
